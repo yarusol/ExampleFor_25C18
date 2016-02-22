de51577b61fcd7c070468b50f0763b16d3cab493
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 19:11:03 +0200

    Adds input of integer value

--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -13,6 +13,17 @@ namespace Project
 			Console.WriteLine( "\n\n\nHello Git ! \n\n" );
 			Console.WriteLine( "I willing to learn it." );
 			Console.WriteLine( "\n\n\n" );
+
+			int val;
+			string input;
+			do
+			{
+				Console.WriteLine( "Enter integer value, please : " );
+				input = Console.ReadLine();
+			} while ( !Int32.TryParse( input, out val ) );
+			Console.WriteLine( "\nYou've entered {0}. It is good integer.\nThank you! ", val );
+
+			Console.WriteLine( "\n\n\n" );
 		}
 	}
 }
