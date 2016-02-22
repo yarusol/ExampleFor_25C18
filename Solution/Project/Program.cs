ce6c58281d82802a2d796f9bbb2a243ee98f31f8
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 18:48:06 +0200

    Initial commit - project created

---
+++ Solution/Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "\n\n\nHello Git ! \n\n\n" );
+		}
+	}
+}
