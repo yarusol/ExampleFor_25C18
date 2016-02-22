66e0c98abde774619300985e9d705d11d40d71f0
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 19:14:30 +0200

    Adds Third message

--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -13,6 +13,9 @@ namespace Project
 			Console.WriteLine( "\n\n\nHello Git ! \n\n" );
 			Console.WriteLine( "I willing to learn it." );
 			Console.WriteLine( "\n\n\n" );
+
+			Console.WriteLine( "Some useful features will be after this. \n\n\n" );
+
 		}
 	}
 }
