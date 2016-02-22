859c76d2faee1ebdd633ae9da94237e57a10fb91
Yarovyy Ruslan <yarusol@gmail.com>
2016-02-22 19:06:08 +0200

    Adds input of double value

--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -13,6 +13,18 @@ namespace Project
 			Console.WriteLine( "\n\n\nHello Git ! \n\n" );
 			Console.WriteLine( "I willing to learn it." );
 			Console.WriteLine( "\n\n\n" );
+
+			double val;
+			string input;
+			do
+			{
+				Console.WriteLine( "Enter double value, please : " );
+				input = Console.ReadLine();
+			} while( ! Double.TryParse( input, out val ) );
+			Console.WriteLine( "\nYou've entered {0}. It is good double.\nThank you! ", val );
+
+			Console.WriteLine( "\n\n\n" );
+
 		}
 	}
 }
